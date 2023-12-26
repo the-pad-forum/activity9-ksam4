8dcd43c6cd92e4bebab3656e992be9052b5a5ac9
Kwesi Sam <samkandoh1230@gmail.com>
2023-12-26 10:42:43 +0000

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: "./app/**/*.{js,ts,jsx,tsx]",
+  // other paths...],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
+
